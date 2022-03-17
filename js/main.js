21bbeb28c9075e16186fcba37563fa6daab58f55
Cheryl Ju <cheryl.as.ju@gmail.com>
2022-03-17 13:33:18 -0400

    Added mobile responsive menu with javascript, css and html.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Testing
 console.log("Testing to make sure JS is working");
+
+
+// Hamburger Menu
+function showMenu () {
+  document.querySelector('.navigation').classList.toggle('active');
+  document.querySelector('.menu').classList.toggle('hide');
+}

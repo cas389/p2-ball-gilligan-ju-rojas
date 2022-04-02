35becc7a8d47503a7256a6bf615ab5b66461402e
Amy Gilligan <agilligan27@gmail.com>
2022-04-02 18:30:52 -0400

    Make Read More buttons responsive with js - still in process

--- js/main.js
+++ js/main.js
@@ -7,3 +7,38 @@ function showMenu () {
   document.querySelector('.navigation').classList.toggle('active');
   document.querySelector('.menu').classList.toggle('hide');
 }
+
+
+// Read More Button
+
+//Button 1
+
+var owButton1 = document.getElementById("ow_button1")
+var hiddenContent = document.getElementById('readmore');
+
+owButton1.addEventListener("click", function(event){
+  event.preventDefault();
+  hiddenContent.style.display = (hiddenContent.style.display === "none") ? "block" :
+  "none";
+}, false);
+
+//Button 2
+var owButton2 = document.getElementById("ow_button2")
+var hiddenContent2 = document.getElementById('readmore2');
+
+owButton2.addEventListener("click", function(event){
+  event.preventDefault();
+  hiddenContent2.style.display = (hiddenContent2.style.display === "none") ? "block" :
+  "none";
+}, false);
+
+//Button 3
+
+var owButton3 = document.getElementById("ow_button3")
+var hiddenContent3 = document.getElementById('readmore3');
+
+owButton3.addEventListener("click", function(event){
+  event.preventDefault();
+  hiddenContent3.style.display = (hiddenContent3.style.display === "none") ? "block" :
+  "none";
+}, false);
